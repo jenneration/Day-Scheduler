0c994ceb15d9fb46e05fcf5eb6ce9bbdf7acf0f5
Jenner Garcia <jenner.garcia@gmail.com>
2020-12-15 22:41:22 -0500

    submission

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Day-Scheduler
+
 Create a day scheduler that load-retrieves to-do's from local storage and colorizes past, present, future time slots.
+
+## CRITERIA MET
+
+- day.js was used to access current date and time for scheduler functionality
+
+- On page load presents
+
+  - A day scheduler with the current day's date displayed at top
+  - And time blocks of regular business day hours
+
+- Each time block is color-coded to indicate if the appt. time has passed, is current, or is in the future.
+
+- User can click on timeblock textarea to enter a "todo" or appointment.
+
+- When save button next to textarea is clicked, entry is saved to local storage.
+
+- On page refresh, saved events persist
+
+- Button is provided to clear local storage and saved events.
+
+## SCREENSHOT
+
+![alt text](assets/dayScheduler.png)
--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-//VARIABLES
-
-// On page load display current day and date
+//Variables
 var now = dayjs();
 console.log(now);
 
@@ -12,7 +10,7 @@ console.log(typeof currentTime);
 var hour = dayjs().format("HH");
 console.log(typeof hour);
 
-//Time slot
+//Time slots
 var apptBlocks = [
   {
     timeMarker: 9,
@@ -65,97 +63,84 @@ var apptBlocks = [
 // var hour = "9 10 11 12 1 2 3 4 5".split(" ");
 // console.log(hour);
 
-//RENDER current day on page load
-$("#currentDay").text(today);
-
 //Add clear button to clear old appts
-// $(clearBtn).on("click", function () {
-//   localStorage.clear();
-//   $(textarea).text("");
-// });
-// function clearTodaysApptBtn {
-//   var clearBtn = $("<button>");
-//   clearBtn.addClass("btn btn-jg btn-secondary");
-//   clearBtn.text("Clear Schedule");
-//   $(".jumbotron").append(clearBtn);
-// }
-
-//GET appts from LS
-init();
+var btnDiv = $("<div>");
+btnDiv.addClass("btnDiv");
+var clearBtn = $("<button>");
+clearBtn.addClass("btn btn-jg btn-secondary");
+clearBtn.text("Clear Schedule");
+btnDiv.append(clearBtn);
+$(".jumbotron").append(btnDiv);
 
 //RENDER Scheduler on page load
-//TODO: ISSUE - when in a function page doubles on click
+function renderScheduler() {
+  //RENDER current day on page
+  $("#currentDay").text(today);
 
-for (var i = 0; i < apptBlocks.length; i++) {
-  //Creates row div for every array object, set "data-hour"
-  var sectionDiv = $("<section>");
-  sectionDiv.addClass("row row-jg time-block");
-  sectionDiv.attr("data-hour", apptBlocks[i].timeMarker);
-  //console.log("data-hour");
-  $(".container").append(sectionDiv);
+  //Create row divs
+  for (var i = 0; i < apptBlocks.length; i++) {
+    //Creates row div for every array object, set "data-hour"
+    var sectionDiv = $("<section>");
+    sectionDiv.addClass("row row-jg time-block");
+    sectionDiv.attr("data-hour", apptBlocks[i].timeMarker);
+    //console.log("data-hour");
+    $(".container").append(sectionDiv);
 
-  //Create column/grid for row
-  for (var b = 0; b < 3; b++) {
-    if (b === 0) {
-      //Hour block
-      var hrBlock = $("<div>");
-      hrBlock.text(apptBlocks[i].hour);
-      // hrBlock.attr(apptBlocks[i].timeMarker);
-      $(sectionDiv).append(hrBlock);
-      hrBlock.addClass("col-2 col-lg-1 hour");
-    } else if (b === 1) {
-      //Text block
-      var textBlock = $("<textarea>");
-      textBlock.text(apptBlocks[i].apptText);
-      textBlock.attr("data-hour", apptBlocks[i].timeMarker);
-      textBlock.attr("data-hour2", hour);
-      textBlock.addClass("col-8 col-lg-10");
+    //Create column/grid for row
+    for (var b = 0; b < 3; b++) {
+      if (b === 0) {
+        //Hour block
+        var hrBlock = $("<div>");
+        hrBlock.text(apptBlocks[i].hour);
+        // hrBlock.attr(apptBlocks[i].timeMarker);
+        $(sectionDiv).append(hrBlock);
+        hrBlock.addClass("col-2 col-lg-1 hour");
+      } else if (b === 1) {
+        //Text block
+        var textBlock = $("<textarea>");
+        textBlock.text(apptBlocks[i].apptText);
+        textBlock.attr("data-hour", apptBlocks[i].timeMarker);
+        textBlock.attr("data-hour2", hour);
+        textBlock.addClass("col-8 col-lg-10 col-8-jg");
 
-      //Create Button
-      var saveBtn = $("<button>");
-      saveBtn.addClass(
-        "col-2 col-lg-1 btn btn-block saveBtn d-flex justify-content-center align-items-center fas fa-save"
-      );
-      saveBtn.attr("data-hour", apptBlocks[i].timeMarker);
-      saveBtn.val(i);
-      //Append elements to sectionDiv
-      $(sectionDiv).append(hrBlock, textBlock, saveBtn);
+        //Create Button
+        var saveBtn = $("<button>");
+        saveBtn.addClass(
+          "col-2 col-lg-1 btn btn-jg btn-block saveBtn d-flex justify-content-center align-items-center fas fa-save"
+        );
+        saveBtn.attr("data-hour", apptBlocks[i].timeMarker);
+        saveBtn.val(i);
+        //Append elements to sectionDiv
+        $(sectionDiv).append(hrBlock, textBlock, saveBtn);
+      }
     }
-  }
+    //Colorize past, present, future
+    var dataHr = $(sectionDiv).attr("data-hour");
 
-  //EXTRA BUTTON TO CLEAR SCHEDULE
-
-  ///////////////COLORIZE TIME BLOCKS//////////////////
-  //Colorize past, present, future
-
-  var dataHr = $(sectionDiv).attr("data-hour");
-
-  if (parseInt(dataHr) === parseInt(currentTime)) {
-    textBlock.addClass("present");
-  } else if (parseInt(dataHr) < parseInt(currentTime)) {
-    textBlock.addClass("past");
-  } else {
-    textBlock.addClass("future");
+    if (parseInt(dataHr) === parseInt(currentTime)) {
+      textBlock.addClass("present");
+    } else if (parseInt(dataHr) < parseInt(currentTime)) {
+      textBlock.addClass("past");
+    } else {
+      textBlock.addClass("future");
+    }
   }
 }
 
-///////////////LOCAL STORAGE FUNCTIONS////////////////
-//GET apps from LS
+//GET apps from localStorage
 function init() {
   storedAppts = JSON.parse(localStorage.getItem("apptBlocks"));
   if (storedAppts !== null) {
     apptBlocks = storedAppts;
   }
-  //RENDER Scheduler
-  //renderScheduler();
 }
 
-//TO STORE
+//SETS items to localStorage
 function storeAppts() {
   localStorage.setItem("apptBlocks", JSON.stringify(apptBlocks));
 }
 
-//BUTTON tied to data-hour - ok
+//BUTTON to target and store new appt entries
 $(".container").on("click", ".saveBtn", function (event) {
   event.preventDefault();
   //Target clicked button in order to target the sibling (textarea) value
@@ -165,6 +150,7 @@ $(".container").on("click", ".saveBtn", function (event) {
   //Grab appt input based on relation to target button
   var appt = target.prev().val(); //this is textarea value
   console.log(appt);
+
   //target clicked button assigned value to target array index item to update
   var targetVal = $(event.target).val();
   console.log(targetVal);
@@ -174,9 +160,18 @@ $(".container").on("click", ".saveBtn", function (event) {
 
   //Store in local storage
   storeAppts();
-
+  //Test time/hour types
   var targetDataHr = target.attr("data-hour");
   console.log(typeof targetDataHr);
   console.log(typeof hour);
 });
-///////////////
+
+//BUTTON to clear current page for new entries, clear LS
+$(clearBtn).on("click", function (event) {
+  event.preventDefault();
+  localStorage.clear();
+  $("textarea").text(" ");
+});
+
+init(); //GET appts from LS
+renderScheduler(); // Render Scheduler
